78de14a9f9c42199d85c6ecb868aefbf7396cf1d
Juan Duran <juanjduranr@gmail.com>
2019-08-11 13:22:08 -0400

    updating scopes

--- src/ProFinder.AuthIdsrv/Config.cs
+++ src/ProFinder.AuthIdsrv/Config.cs
@@ -1,10 +1,12 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
+using IdentityModel;
 using IdentityServer4;
 using IdentityServer4.Models;
 using IdentityServer4.Test;
 using System.Collections.Generic;
+using System.Security.Claims;
 
 namespace ProFinder.AuthIdsrv
 {
@@ -12,9 +14,15 @@ namespace ProFinder.AuthIdsrv
     {
         public static IEnumerable<IdentityResource> GetIdentityResources()
         {
+            var customProfile = new IdentityResource(
+            name: "custom.profile",
+            displayName: "Custom Profile",
+            claimTypes: new[] { JwtClaimTypes.PreferredUserName });
+
             return new IdentityResource[]
             {
-                new IdentityResources.OpenId()
+                new IdentityResources.OpenId(),
+                customProfile
             };
         }
 
@@ -22,7 +30,7 @@ namespace ProFinder.AuthIdsrv
         {
             return new List<ApiResource>
             {
-                new ApiResource("profinder", "Pro Finder Inc.")
+                new ApiResource("profinder", "Pro Finder Inc.", new[] { JwtClaimTypes.PreferredUserName })
             };
         }
 
@@ -39,7 +47,13 @@ namespace ProFinder.AuthIdsrv
                     {
                         new Secret("Ub9efngDat_Evrzn6DM8D_Qm7uNJmsPT".Sha256())
                     },
-                    AllowedScopes = { "profinder", IdentityServerConstants.StandardScopes.OfflineAccess }
+                    AllowedScopes =
+                    {
+                        "profinder",
+                        IdentityServerConstants.StandardScopes.OfflineAccess,
+                        IdentityServerConstants.StandardScopes.OpenId
+                    },
+                    AlwaysIncludeUserClaimsInIdToken = true
                 }
             };
         }
@@ -52,7 +66,11 @@ namespace ProFinder.AuthIdsrv
                 {
                     SubjectId = "1",
                     Username = "user01",
-                    Password = "password"
+                    Password = "password",
+                    Claims = new []
+                    {
+                        new Claim("preferred_username", "Juan Duran")
+                    }
                 }
             };
         }
--- src/ProFinder.WebAPI/Startup.cs
+++ src/ProFinder.WebAPI/Startup.cs
@@ -44,7 +44,9 @@ namespace ProFinder.WebAPI
             }
 
             app.UseCors(
-                options => options.WithOrigins("http://localhost:3000").AllowAnyMethod()
+                options => options.WithOrigins("http://localhost:3000")
+                                  .AllowAnyMethod()
+                                  .AllowAnyHeader()
             );
 
             app.UseMvc();
